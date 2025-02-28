ee36da91827b630d5f3b09e5390e6e9cdbfe9d62
Alessandro-Incantalupo <alessandro.incantalupo@gmail.com>
2025-02-28 17:28:30 +0100

    feat:  tailwind and primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Button, ButtonDirective, ButtonIcon, ButtonLabel } from 'primeng/button';
+import { Select } from 'primeng/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Textarea } from 'primeng/textarea';
+import { Checkbox } from 'primeng/checkbox';
+import { WindowMaximizeIcon } from 'primeng/icons';
+import { Ripple } from 'primeng/ripple';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Button,
+    Select,
+    FormsModule,
+    Textarea,
+    Checkbox,
+    ReactiveFormsModule,
+    WindowMaximizeIcon,
+    Ripple,
+    ButtonDirective,
+    ButtonIcon,
+    ButtonLabel,
+    RouterLink,
+  ],
   templateUrl: './app.component.html',
   styles: [],
 })
