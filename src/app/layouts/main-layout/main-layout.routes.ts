babd2d92058e76d600dab3c5c7864567ab8e2e12
Alessandro-Incantalupo <alessandro.incantalupo@gmail.com>
2025-02-28 21:44:14 +0100

    feat: main layout and basic routing

---
+++ src/app/layouts/main-layout/main-layout.routes.ts
@@ -0,0 +1,14 @@
+import {Routes} from '@angular/router';
+import {MainLayoutComponent} from './main-layout.component';
+
+// Exporting the default array of routes for the MainLayoutComponent
+// This array includes a route with an empty path that uses MainLayoutComponent
+// and allows for potential child routes to be added under 'children'.
+// Using `export default` ensures that `loadChildren` can dynamically import this module correctly.
+export default [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: []
+  }
+] satisfies Routes;
