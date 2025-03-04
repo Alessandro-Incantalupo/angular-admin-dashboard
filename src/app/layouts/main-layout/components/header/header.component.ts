2ce9d15a90460fc1dceaf1d83d30b72e5ec6e9ac
Alessandro-Incantalupo <alessandro.incantalupo@gmail.com>
2025-03-04 18:09:24 +0100

    feat: implemented menu service and navbar submenu

--- src/app/layouts/main-layout/components/header/header.component.ts
+++ src/app/layouts/main-layout/components/header/header.component.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
 import { SvgIconComponent } from 'angular-svg-icon';
 import { NavbarMenuComponent } from './navbar-menu/navbar-menu.component';
+import { ProfileMenuComponent } from './profile-menu/profile-menu.component';
 
 @Component({
   selector: 'app-header',
-  imports: [
-    SvgIconComponent,
-    NavbarMenuComponent,
-  ],
+  imports: [SvgIconComponent, NavbarMenuComponent, ProfileMenuComponent],
   templateUrl: './header.component.html',
   styles: ``,
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
