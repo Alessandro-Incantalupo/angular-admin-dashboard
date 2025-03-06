3e7de1ed62ca8dd9cd1a8f17fc7be21a0379b544
Alessandro-Incantalupo <alessandro.incantalupo@gmail.com>
2025-03-06 15:43:58 +0100

    feat: siderbar menu completion

--- src/app/layouts/main-layout/components/sidebar/sidebar.component.ts
+++ src/app/layouts/main-layout/components/sidebar/sidebar.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { SvgIconComponent } from 'angular-svg-icon';
 import { MenuService } from '../../services/menu.service';
 import { APP_INFO } from '../../../../core/tokens/version.token';
+import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [SvgIconComponent],
+  imports: [SvgIconComponent, SidebarMenuComponent],
   templateUrl: './sidebar.component.html',
   styles: ``,
 })
