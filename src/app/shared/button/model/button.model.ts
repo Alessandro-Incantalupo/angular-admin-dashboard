21edc47730f1abdc7131855a6e21e7804f2ccdeb
Alessandro-Incantalupo <alessandro.incantalupo@gmail.com>
2025-03-06 16:18:49 +0100

    feat: added error page and button

---
+++ src/app/shared/button/model/button.model.ts
@@ -0,0 +1,7 @@
+export interface ButtonProps {
+  impact: 'bold' | 'light' | 'none';
+  size: 'small' | 'medium' | 'large';
+  shape: 'square' | 'rounded' | 'pill';
+  tone: 'primary' | 'danger' | 'success' | 'warning' | 'info' | 'light';
+  shadow: 'none' | 'small' | 'medium' | 'large';
+}
