6badba5e91b838ae72f5f2a51d3a710320ca41f7
Alessandro-Incantalupo <alessandro.incantalupo@gmail.com>
2025-03-10 11:22:46 +0100

    feat: adding table

---
+++ src/app/features/ui/table/table.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  imports: [],
+  templateUrl: './table.component.html',
+  styles: ``,
+})
+export default class TableComponent {}
