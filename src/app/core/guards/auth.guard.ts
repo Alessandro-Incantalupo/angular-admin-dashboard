e812905269a4b285bc1b8c7ba959759ca32a1e5a
Alessandro-Incantalupo <alessandro.incantalupo@gmail.com>
2025-03-11 10:24:15 +0100

    refactor: added working guard and related logic

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,14 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../../features/auth/services/auth.service';
+
+export const AuthGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (!authService.isAuthenticated()) {
+    router.navigate(['/auth/sign-up']); // Redirect if not logged in
+    return false;
+  }
+  return true; // Allow access if authenticated
+};
--- src/app/features/auth/sign-up/sign-up.component.ts
+++ src/app/features/auth/sign-up/sign-up.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { SvgIconComponent } from 'angular-svg-icon';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [SvgIconComponent, RouterLink, ButtonComponent],
+  imports: [SvgIconComponent, RouterLink, ButtonComponent, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styles: ``,
 })
-export default class SignUpComponent {}
+export default class SignUpComponent {
+  nnfb = inject(NonNullableFormBuilder);
+  router = inject(Router);
+  authService = inject(AuthService);
+
+  isLoading = signal(false);
+
+  // Reactive Form
+  form = this.nnfb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    confirmPassword: ['', [Validators.required]],
+  });
+
+  passwordsMatch = computed(() => this.form.value.password === this.form.value.confirmPassword);
+
+  register() {
+    if (this.form.invalid) return; // Stop if invalid form
+
+    if (this.form.value.password !== this.form.value.confirmPassword) {
+      alert('Passwords do not match');
+      return;
+    }
+
+    this.isLoading.set(true);
+    localStorage.setItem('token', 'fake-jwt-token'); // Simulate authentication
+    setTimeout(() => {
+      this.router.navigate(['/profile']);
+    }, 1000);
+  }
+
+  // Helper function to check form field validity
+  hasError(controlName: string, errorType: string): boolean | undefined {
+    const control = this.form.get(controlName);
+    return control?.hasError(errorType) && control.touched;
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/auth/sign-up']); // Redirect to login after logout
+  }
+}
