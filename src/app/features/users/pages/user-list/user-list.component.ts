30c8bb9b7588aa6114c093f252bff9ee74839f60
Alessandro-Incantalupo <alessandro.incantalupo@gmail.com>
2025-04-28 16:14:32 +0200

    feat:  added user table component

--- src/app/features/users/pages/user-list/user-list.component.ts
+++ src/app/features/users/pages/user-list/user-list.component.ts
@@ -1,10 +1,17 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  input,
+} from '@angular/core';
 import { TranslocoDirective } from '@jsverse/transloco';
 import { UsersStore } from '@features/users/state/user.store';
+import { User } from '@features/users/models/user.model';
+import { UserTableComponent } from '@features/users/components/user-table/user-table.component';
 
 @Component({
   selector: 'app-user-list',
-  imports: [TranslocoDirective],
+  imports: [TranslocoDirective, UserTableComponent],
   templateUrl: './user-list.component.html',
   styles: `
     :host {
@@ -15,4 +22,11 @@ import { UsersStore } from '@features/users/state/user.store';
 })
 export default class UserListComponent {
   protected userStore = inject(UsersStore);
+  onEdit(user: User) {
+    console.log('Edit user:', user);
+  }
+
+  onDelete(user: User) {
+    console.log('Delete user:', user);
+  }
 }
